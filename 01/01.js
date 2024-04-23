84bc93d66daadb0e9f45fc00a88988b6cb2b6e00
pnumin <pnumin@pusan.ac.kr>
2024-04-23 17:42:54 +0900

    자바스크립트 클릭이벤트

---
+++ 01/01.js
@@ -0,0 +1,11 @@
+function handleClick(n) {
+  // document.querySelector('#msg').innerHTML = '<h1>안녕하세요!' + n + '</h1>' ;
+  document.querySelector('#msg').innerHTML = `<h1>안녕하세요! ${n}</h1>` ;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  // document.getElementById('msg').innerHTML = '안녕하세요' ;
+  // document.querySelector('#msg').innerHTML = '<h1>안녕하세요</h1>' ;
+}) ;
